69e2cac3d6fbf534c300676166359b31766a6e7e
confusdcodr <confusdcodr@users.noreply.github.com>
2020-01-07 17:09:22 -0500

    Add ability to create basic metric alarms

--- variables.tf
+++ variables.tf
@@ -1,1 +1,11 @@
+variable "create_metric_alarm" {
+  description = "Controls whether to create the CloudWatch metric alarm(s)"
+  type        = bool
+  default     = true
+}
 
+variable "metric_alarms" {
+  description = "Schema list containing the fields noted in the Variable Defintions sections"
+  type        = list
+  default     = []
+}
